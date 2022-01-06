608070f6321c59668db8a481c070cec0a2a9c8f0
Ravi Adabala <ravikumar.adabala@gmail.com>
2022-01-06 22:17:34 +0530

    Added nav bar component and bootswatch theme

--- src/WooingUi/src/app/app.module.ts
+++ src/WooingUi/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
